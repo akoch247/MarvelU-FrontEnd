686aa234d994ee1ca75676339284ce85059cde0a
Adriana Koch <akoch@oberlin.edu>
2025-07-18 17:11:51 -0400

    finish dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Error404 from './Error404.jsx';
 import Login from "./login/Login.jsx";
 import Register from './login/Register.jsx';
 import FacultyPage from './faculty/FacultyPage.jsx';
+import SingleFaculty from './faculty/SingleFaculty.jsx';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={<Layout />} >
           <Route index element={<Login />} />
           <Route path="/faculty" element={<FacultyPage />} />
+          <Route path="faculty/:id" element={<SingleFaculty />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="*" element={<Error404 />} />
--- src/faculty/AllFaculty.jsx
+++ src/faculty/AllFaculty.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Faculty.css";
-
+import { professors } from "../data/DummyData";
+import { Link } from "react-router-dom";
+3
 export default function AllFaculty() {
-    <>
-        <h1>All Faculty</h1>
-    </>
+    return (
+        <div>
+            <h1>All Faculty</h1>
+            {professors.map((prof) => (
+                <div key={prof.id}>
+                    <Link to={`/faculty/${prof.id}`}>
+                        <h2>{prof.name}</h2>
+                    </Link>
+                    <img src={prof.profile_img} alt={prof.name} />
+                </div>
+            ))}
+        </div>
+    );
 }
