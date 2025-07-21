8ecbebd8bc150786f1066c6ce1b87e07121946a6
Knox Wong <wongknox@gmail.com>
2025-07-21 16:09:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import './App.css'
-import { Routes, Route } from "react-router-dom";
-import Layout from "./layout/Layout.jsx";
-import Error404 from './Error404.jsx';
-import Login from "./login/Login.jsx";
-import Register from './login/Register.jsx';
-import FacultyPage from './faculty/FacultyPage.jsx';
-import SingleFaculty from './faculty/SingleFaculty.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import Error404 from "./Error404.jsx";
+import Navbar from "./layout/Navbar.jsx";
+import FacultyPage from "./faculty/FacultyPage.jsx";
+import SingleFaculty from "./faculty/SingleFaculty.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-
-
-function App() {
-
+function Layout() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />} >
+    <>
+      <Navbar />
+      <div className="container">
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Login />} />
           <Route path="/faculty" element={<FacultyPage />} />
           <Route path="faculty/:id" element={<SingleFaculty />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="*" element={<Error404 />} />
-      </Route>
-    </Routes>
-  )
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
