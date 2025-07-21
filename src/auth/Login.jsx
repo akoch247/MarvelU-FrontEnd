24255a2f212dfee622cf3391699c0c15e1eae6cd
Knox Wong <wongknox@gmail.com>
2025-07-21 18:11:13 -0500

    Update Login.jsx and Register.jsx

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,10 +1,41 @@
 import React from "react";
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
 export default function Login() {
-    return (
-        <>
-            <h1>Login</h1>
-        </>
-    );
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const [error, setError] = useState(null);
+
+  const tryLogin = async (formData) => {
+    const username = formData.get("username");
+    const password = formData.get("password");
+    try {
+      await login({ username, password });
+      navigate("/activities");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Log in to your account</h1>
+      <form action={tryLogin}>
+        <label>
+          Username
+          <input type="text" name="username" required />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" required />
+        </label>
+        <button>Login</button>
+        {error && <output>{error}</output>}
+      </form>
+      <Link to="/register">Need an account? Register here.</Link>
+    </>
+  );
 }
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
 export default function Register() {
-    return (
-        <>
-            <h1>Register</h1>
-        </>
-    );
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const [error, setError] = useState(null);
+
+  const tryRegister = async (formData) => {
+    const username = formData.get("username");
+    const password = formData.get("password");
+    try {
+      await register({ username, password });
+      navigate("/activities");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Register for an account</h1>
+      <form action={tryRegister}>
+        <label>
+          Username
+          <input type="text" name="username" required />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" required />
+        </label>
+        <button>Register</button>
+        {error && <output>{error}</output>}
+      </form>
+      <Link to="/login">Already have an account? Log in here.</Link>
+    </>
+  );
 }
